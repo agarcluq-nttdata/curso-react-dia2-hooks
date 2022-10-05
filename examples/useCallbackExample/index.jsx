4c2fbb38bdf9cc2af49c67bb6d426773210ad769
agarcluq-nttdata <115019846+agarcluq-nttdata@users.noreply.github.com>
2022-10-05 10:02:05 +0200

    1 file modified

--- examples/useCallbackExample/index.jsx
+++ examples/useCallbackExample/index.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useCallback, useState } from 'react'
 
 const Button = React.memo(({handleClick, name}) => {
@@ -9,11 +10,12 @@ const UseCallbackExample = () => {
   console.log('counter rendered')
   const [countOne, setCountOne] = useState(0)
   const [countTwo, setCountTwo] = useState(0)
-  const memoizedSetCountOne = useCallback(() => setCountOne(countOne + 1), [countOne)
+  const memoizedSetCountOne = useCallback(() => setCountOne(countOne + 1), [countOne])
   const memoizedSetCountTwo = useCallback(() => setCountTwo(countTwo + 1), [countTwo])
   return (
     <>
-        {countOne} {countTwo}
+        <h2>UseCallback</h2>
+        <h3>{countOne} {countTwo}</h3>
         <Button handleClick={memoizedSetCountOne} name="button1" />
         <Button handleClick={memoizedSetCountTwo} name="button1" />
     </>
